1f72488851b1785f7e1f9ccf09432058ddab5b43
kaw393939 <keith@webizly.com>
2022-02-03 10:31:05 -0500

    Changed the name on the homepage

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Keith"
+        name = "Keith Williams"
         return render_template('index.html', name=name)
