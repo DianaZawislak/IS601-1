3d0a8851a35083ec772b00a2ec133a7cf6b905b7
Diana Zawislak <dianazawislak@dianas-mbp.myfiosgateway.com>
2022-02-07 22:25:26 -0500

    changed the name in the controller

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Diana Zawislak"
         return render_template('index.html', name=name)
